168cb16d137eb275c06c4abd766cad62168b0dbe
cseitz5 <cseitz5@kent.edu>
2020-11-30 14:39:04 -0500

    ignore case because windows dumb

---
+++ src/views/dashboard/Apply.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="apply">
+    Apply to KHE
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Apply',
+  components: {
+
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/views/dashboard/Apply.scss"; // Essential SCSS
+
+
+
+</style>
---
+++ src/views/dashboard/Landing.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="landing">
+    Landing page for Hacker Dashboard
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Landing',
+  components: {
+
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/views/dashboard/Landing.scss"; // Essential SCSS
+
+
+
+</style>
