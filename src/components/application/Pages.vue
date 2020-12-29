fa32797e4e169a3ced7ff58eda52ea9ff9f66493
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2020-12-29 16:50:22 -0500

    Application pages WIP

---
+++ src/components/application/Pages.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="application-pages">
+    yea boiiiii
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ApplicationPages',
+
+};
+</script>
+
+<style scoped lang="scss">
+//@import "@/scss/components/FAQ.scss"; // Essential SCSS
+
+
+</style>
