24c35ee0bb7214f2faf1b520f56bd30475571f46
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2021-03-01 12:44:50 -0500

    backend exports

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import DatabaseClient from '@/db';
+let { ApplicationSchema } = DatabaseClient;
+console.log({ ApplicationSchema })
+
+
+createApp(App).use(store).use(router).mount('#app');
