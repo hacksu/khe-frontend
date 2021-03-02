88ba3ebc56943114e06b68e8444fa74c7305e70d
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2021-03-01 20:04:32 -0500

    dashboard routes

--- src/router/index.js
+++ src/router/index.js
@@ -14,12 +14,42 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/dashboard',
+    component: () => import(/* webpackChunkName: "dashboard" */ '../views/Dashboard.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Dashboard',
+        component: () => import(/* webpackChunkName: "dashboard" */ '../views/dashboard/Landing.vue'),
+      },
+      {
+        path: 'apply',
+        name: 'Apply',
+        component: () => import(/* webpackChunkName: "dashboard" */ '../views/dashboard/Apply.vue'),
+      }
+    ]
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.path == '/signup/confirm_email') {
+    next({
+      path: '/api/user/confirm_email',
+      query: {
+        code: to.query.code,
+        redirect: '/dashboard/apply',
+      },
+    })
+  } else {
+    next();
+  }
 })
 
 export default router
